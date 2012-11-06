af88f5a3fbd4f50feb06ae12cc73e77c6c65f340
Jason Kelly <jkelly@cambridge-intl.com>
2012-11-06 14:38:51 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FizzBuzz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(12, 12);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Do FizzBuzz";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(12, 41);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOut.Size = new System.Drawing.Size(448, 248);
+            this.txtOut.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 301);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "FizzBuzz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtOut;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FizzBuzz
+{
+    // Write a program that prints the numbers from 1 to 100. 
+    // But for multiples of three print “Fizz” instead of the number 
+    // and for the multiples of five print “Buzz”. For numbers which 
+    // are multiples of both three and five print “FizzBuzz”.
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            DoFizzBuzz();
+        }
+
+        private void DoFizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string s = "";
+                if (i % 3 == 0)
+                    s += "Fizz";
+                if (i % 5 == 0)
+                    s += "Buzz";
+
+                if (string.IsNullOrEmpty(s))
+                    s = i.ToString();
+
+                txtOut.Text += s + System.Environment.NewLine;
+            }
+        }
+    }
+}
